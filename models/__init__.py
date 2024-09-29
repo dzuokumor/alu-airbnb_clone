ca80a7e000df14b02a0fc30c263b3efc817b5de7
dzuokumor <d.zuokumor@alustudent.com>
2024-09-29 10:09:40 -0700

    debug errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""Initializes the storage system for the Airbnb clone project"""
+
+from models.engine.file_storage import FileStorage
+
+# Create the `storage` instance and load existing data from the file.
+storage = FileStorage()
+storage.reload()
 
