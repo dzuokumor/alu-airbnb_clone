724fea319b2a8d7934dfad4e0fda13b8faad3c6e
dzuokumor <d.zuokumor@alustudent.com>
2024-09-29 10:41:08 -0700

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+
+"""This file defines the UserModel class
+It inherits from the BaseModel
+"""
+
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
+    """The User Model"""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a User instance."""
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
-    def __str__(self):
-        """Return a string representation of the User instance."""
-        return f"[User] ({self.id}) {self.__dict__}"
+    # Attributes
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
